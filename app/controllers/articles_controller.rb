27a2323eb90c21d42650d260fb0ca83776574832
Masaaki-Tanikawa <m.tani571215@gmail.com>
2025-05-03 17:27:58 +0800

    記事詳細ページの作成・一覧ページに記事リンクを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all # Articleモデルのすべての記事を取得して@articles(複数取得するため@articleから変更)に代入(インスタンス化)する
   end
+
+	def show
+		@article = Article.find(params[:id]) #Articleのidを取得する
+	end
 end
