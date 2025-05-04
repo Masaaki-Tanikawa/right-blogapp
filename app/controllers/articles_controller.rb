480500e652dc65964332e55deaca0bc51601b871
Masaaki-Tanikawa <m.tani571215@gmail.com>
2025-05-04 15:00:04 +0800

    記事作成ページに保存機能を追加＋flashメッセージの表示・日本語化

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,20 @@ class ArticlesController < ApplicationController
 	def new
 		@article = Article.new #空のArticleインスタンスを作成する
 	end
+
+	def create
+		@article = Article.new(article_params) #@article(new.html.erb)に、article_paramsの情報を保存するための箱を作る
+		if @article.save #データベースに値を保存する
+			redirect_to article_path(@article), notice: '保存できました' #記事詳細ページにジャンプする（「保存できました」と表示）
+		else
+			flash.now[:error] = '保存に失敗しました'
+			render :new, status: :unprocessable_entity #@article(:title, :content)のデータを保存した状態で、new.html.erbを表示
+		end
+	end
+
+	private #Strong Parameterを書くときに入力
+	def article_params
+		params.require(:article).permit(:title, :content) #フォームから投稿されたデータより、:titleと:contentの情報を抜き取る
+		# params:Strong Parameterのひとつで、データを変更されても保存しないようにする。:titleと:contentだけ保存を許可
+	end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+	validates :title, presence: true #presence: trueで入力を必須にする
+	validates :content, presence: true
 end
