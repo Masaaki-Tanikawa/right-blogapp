d7167a2f52b21f5aff4e66f61bd439d887ed178b
Masaaki-Tanikawa <m.tani571215@gmail.com>
2025-08-30 18:01:57 +0800

    ActionTextの導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,6 @@
 # Table name: articles
 #
 #  id         :integer          not null, primary key
-#  content    :text             not null
 #  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -23,13 +22,14 @@ class Article < ApplicationRecord
   validates :title, format: { with: /\A(?!\@)/ } # format: で先頭に@をつけることを禁止する
 
   validates :content, presence: true
-  validates :content, length: { minimum: 10 }
-  validates :content, uniqueness: true	# uniqueness: で値を一意（ユニーク）に指定する(uniquenessはSNSアカウント・メールアドレスでも使用)
+  # validates :content, length: { minimum: 10 }
+  # validates :content, uniqueness: true	# uniqueness: で値を一意（ユニーク）に指定する(uniquenessはSNSアカウント・メールアドレスでも使用)
 
   has_many :comments, dependent: :destroy # 1つの記事に対して複数のコメントを紐づける(記事が削除されたらコメントも削除)
   belongs_to :user # articleを1つのユーザーに紐づける
   has_many :likes, dependent: :destroy # 1つの記事に対して複数のいいねを紐づける(記事が削除されたらいいねも削除)
   has_one_attached :eyecatch # アイキャッチ画像を（1つ）アップロード
+  has_rich_text :content # contentの値をエディタで強化して保存(その後、既存のcontentを削除)
 
   def display_created_at
     I18n.l(self.created_at, format: :default) # 日付表示における冗長な部分を共通化
